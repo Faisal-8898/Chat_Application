27e60f83917120eaaa57fbb2b5211cb730d630f0
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-04-29 22:44:10 +0600

    updating client

--- src/Server/Client.java
+++ src/Server/Client.java
@@ -1,5 +1,7 @@
 package Server;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -9,40 +11,54 @@ public class Client {
     private Socket socket;
     private BufferedReader br;
     private PrintWriter out;
+    private String userId;
 
-
-    public Client() {
+    public Client(String userId) {
+        this.userId = userId;
         try {
-            System.out.println("Sending request to Faisal's server...");
-            socket = new Socket("127.0.0.1", 7778);
-            System.out.println("Connection done");
+            System.out.println("Connecting to server...");
+            socket = new Socket("localhost", 7778);
+            System.out.println("Connection established.");
 
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream());
+
+            // send user id to server
+            out.println(userId);
+            out.flush();
+
             startReading();
             startWriting();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error connecting to server: " + e.getMessage());
         }
     }
 
     public void startReading() {
         // Thread for reading
         Runnable r1 = () -> {
-            System.out.println("Reader started...");
+            System.out.println("Reader started.");
 
             try {
                 String msg;
                 while ((msg = br.readLine()) != null) {
                     if (msg.equals("exit")) {
-                        System.out.println("Server has terminated the chat");
+                        System.out.println("Server has terminated the chat.");
                         socket.close();
                         break;
                     }
                     System.out.println("Server: " + msg);
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("Error while reading: " + e.getMessage());
+            } finally {
+                try {
+                    if (socket != null && !socket.isClosed()) {
+                        socket.close();
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error while closing the socket: " + e.getMessage());
+                }
             }
         };
         new Thread(r1).start();
@@ -51,28 +67,36 @@ public class Client {
     public void startWriting() {
         // Thread for writing and sending to server
         Runnable r2 = () -> {
-            System.out.println("Writer Started...");
+            System.out.println("Writer started.");
             try {
                 BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
                 String content;
                 while ((content = br1.readLine()) != null) {
-                    out.println(content);
+                    out.println(userId + ": " + content);
                     out.flush();
                     if (content.equals("exit")) {
-                        System.out.println("Server has terminated the chat");
+                        System.out.println("Server has terminated the chat.");
                         socket.close();
                         break;
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("Error while writing: " + e.getMessage());
+            } finally {
+                try {
+                    if (socket != null && !socket.isClosed()) {
+                        socket.close();
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error while closing the socket: " + e.getMessage());
+                }
             }
         };
         new Thread(r2).start();
     }
 
     public static void main(String[] args) {
-        System.out.println("This is client responding");
-        new Client();
+        String userId = "user123"; // replace with user id of your choice
+        new Client(userId);
     }
 }
