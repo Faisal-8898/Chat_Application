acc1e4dc3ea03370f36f9444204a26dbfed3d608
Faisal Ahmed <125754474+Faisal-8898@users.noreply.github.com>
2023-04-29 21:33:21 +0600

    Update Database.java

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -5,9 +5,9 @@ import java.util.*;
 
 public class Database {
     
-    private MongoDatabase database;
+private MongoDatabase database;
     
-    public UserManager(MongoDatabase database) {
+    public Database(MongoDatabase database) {
         this.database = database;
     }
     
@@ -19,8 +19,27 @@ public class Database {
                 Updates.set("email", email),
                 Updates.set("password", password)));
         System.out.println("User with userId " + userId + " has been updated.");
+        
+        // Update the senderId in the messages collection
+        MongoCollection<Document> messagesCollection = database.getCollection("messages");
+        Document messagesFilter = new Document("senderId", userId);
+        Document messagesUpdate = new Document("$set", new Document("senderId", userId));
+        messagesCollection.updateMany(messagesFilter, messagesUpdate);
     }
     
-    
-    
+    public void insertMessage(String messageId, String senderId, String recipientId, String message, String type, String status) {
+        MongoCollection<Document> collection = database.getCollection("messages");
+        Document doc = new Document("messageId", messageId)
+            .append("senderId", senderId)
+            .append("recipientId", recipientId)
+            .append("message", message)
+            .append("type", type)
+            .append("status", status)
+            .append("timestamp", new Date());
+        collection.insertOne(doc);
+        System.out.println("Message with ID " + messageId + " sent from " + senderId + " to " + recipientId + " saved in the database.");
+    }
+
 }
+
+   
