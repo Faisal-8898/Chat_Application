db7815ad78d7b1efd033dc3bc02a0f76bf2a56f9
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-05-05 01:25:00 +0600

    user added

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -14,14 +14,7 @@ private MongoDatabase database;
         this.database = database;
     }
     
-    public void updateUser(String userId, String name, String email, String password) {
-        MongoCollection<Document> collection = database.getCollection("users");
-        collection.updateOne(Filters.eq("userId", userId),
-            Updates.combine(
-                Updates.set("name", name),
-                Updates.set("email", email),
-                Updates.set("password", password)));
-        System.out.println("User with userId " + userId + " has been updated.");
+
         
         // Update the senderId in the messages collection
         MongoCollection<Document> messagesCollection = database.getCollection("messages");
