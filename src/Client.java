07c7fd8117e00873fb62572a7de04250f26c2a52
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-05-19 20:37:37 +0600

    updating the whole thing

---
+++ src/Client.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+    private PrintWriter outputStream;
+    private BufferedReader inputStream;
+
+    public Client(String serverAddress, int serverPort) {
+        try {
+            socket = new Socket(serverAddress, serverPort);
+            outputStream = new PrintWriter(socket.getOutputStream(), true);
+            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        // Create a separate thread to read messages from the server
+        Thread readerThread = new Thread(new ServerReader());
+        readerThread.start();
+
+        try {
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+            // Prompt for a username
+            System.out.print("Enter your username: ");
+            String username = consoleReader.readLine();
+
+            // Send the username to the server
+            outputStream.println(username);
+
+            String message;
+            while ((message = consoleReader.readLine()) != null) {
+                if (message.equals("/ul")) {
+                    outputStream.println(message);
+                } else if (message.startsWith("/talk")) {
+                    String[] parts = message.split(" ");
+                    if (parts.length >= 3) {
+                        String receiverIndex = parts[1];
+                        String receiverMessage = message.substring(message.indexOf(receiverIndex) + receiverIndex.length() + 1);
+                        outputStream.println("/talk " + receiverIndex + " " + receiverMessage);
+                    } else {
+                        System.out.println("Invalid command. Please use the format: /talk <receiverIndex> <message>");
+                    }
+                } else {
+                    outputStream.println(message);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+    }
+
+    private void closeResources() {
+        try {
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ServerReader implements Runnable {
+        @Override
+        public void run() {
+            try {
+                String message;
+                while ((message = inputStream.readLine()) != null) {
+                    if (message.startsWith("Connected Users:")) {
+                        System.out.println(message);
+                    } else {
+                        System.out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                closeResources();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String serverAddress = "localhost"; // Replace with the server address
+        int serverPort = 1234; // Replace with the server port
+
+        Client client = new Client(serverAddress, serverPort);
+        client.start();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,151 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private boolean running;
+    private List<ClientHandler> clients;
+
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            running = true;
+            clients = new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        System.out.println("Server started.");
+
+        while (running) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                // Create a new thread or process to handle the client communication
+                ClientHandler clientHandler = new ClientHandler(clientSocket, this);
+                clients.add(clientHandler);
+                clientHandler.start();
+
+                sendConnectedUsersToAllClients();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void addClient(ClientHandler client) {
+        clients.add(client);
+        sendConnectedUsersToAllClients();
+    }
+
+    public synchronized void removeClient(ClientHandler client) {
+        clients.remove(client);
+        sendConnectedUsersToAllClients();
+    }
+
+    public synchronized void sendConnectedUsersToAllClients() {
+        StringBuilder userList = new StringBuilder("Connected Users:\n");
+        for (int i = 0; i < clients.size(); i++) {
+            ClientHandler client = clients.get(i);
+            userList.append(i).append(". ").append(client.getUsername()).append("\n");
+        }
+
+        for (ClientHandler client : clients) {
+            client.sendMessage(userList.toString());
+        }
+    }
+
+    public synchronized void sendMessageToClient(ClientHandler sender, int receiverIndex, String message) {
+        if (receiverIndex >= 0 && receiverIndex < clients.size()) {
+            ClientHandler client = clients.get(receiverIndex);
+            client.sendMessage("[" + sender.getUsername() + "]: " + message);
+        } else {
+            sender.sendMessage("Invalid receiver index.");
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 1234; // Specify your desired port number
+        Server server = new Server(port);
+        server.start();
+    }
+
+    private class ClientHandler extends Thread {
+        private Socket socket;
+        private BufferedReader inputStream;
+        private PrintWriter outputStream;
+        private Server server;
+        private String username;
+
+        public ClientHandler(Socket socket, Server server) {
+            this.socket = socket;
+            this.server = server;
+            try {
+                inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                outputStream = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void sendMessage(String message) {
+            outputStream.println(message);
+        }
+
+        @Override
+        public void run() {
+            try {
+                username = inputStream.readLine();
+
+                // Notify all clients about the new user
+                server.sendConnectedUsersToAllClients();
+
+                String message;
+                while ((message = inputStream.readLine()) != null) {
+                    if (message.startsWith("/talk")) {
+                        String[] parts = message.split(" ");
+                        if (parts.length >= 3) {
+                            int receiverIndex = Integer.parseInt(parts[1]);
+                            String receiverMessage = message.substring(message.indexOf(parts[2]));
+                            server.sendMessageToClient(this, receiverIndex, receiverMessage);
+                        } else {
+                            sendMessage("Invalid command. Please use the format: /talk <receiverIndex> <message>");
+                        }
+                    } else {
+                        server.sendConnectedUsersToAllClients();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                // Remove the client from the server's list and notify all clients
+                server.removeClient(this);
+                server.sendConnectedUsersToAllClients();
+                closeResources();
+            }
+        }
+
+        private void closeResources() {
+            try {
+                inputStream.close();
+                outputStream.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
